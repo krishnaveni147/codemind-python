6009a9d7199de25f0753c87796bfa28135232dc8
krishnaveni147 <99851071+krishnaveni147@users.noreply.github.com>
2023-06-02 06:21:07 +0530

    m, n = map(int, input().split()) if (n > m):     t = n else:     t = m i = 1 while i != 0:     if(i*t)%m == 0 and (i*t)%n == 0:         print(i * t)         break     i += 1

---
+++ LCM.py
@@ -0,0 +1,13 @@
+m, n = map(int, input().split())
+if (n > m):
+    t = n
+else:
+    t = m
+i = 1
+while i != 0:
+    if(i*t)%m == 0 and (i*t)%n == 0:
+        print(i * t)
+        break
+    i += 1
+
+     
